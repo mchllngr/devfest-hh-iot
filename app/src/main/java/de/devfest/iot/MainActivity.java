c1f6dfaacb30158d93e155b54d47b7c0d7eed0f9
Michael Langer <dev@mlanger.net>
2017-11-24 00:48:06 +0100

    remove implementation

--- app/src/main/java/de/devfest/iot/MainActivity.java
+++ app/src/main/java/de/devfest/iot/MainActivity.java
@@ -4,17 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.things.contrib.driver.button.Button;
-import com.google.android.things.pio.Gpio;
-import com.google.android.things.pio.PeripheralManagerService;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.io.IOException;
-
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -26,11 +15,6 @@ public class MainActivity extends Activity {
     private static final String GPIO_LED_PIN_NAME = "GPIO_10";
 
     // TODO 02: get a FirebaseDatabase instance
-    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference databaseReference;
-    private Button button;
-    private Gpio led;
-    private ValueEventListener valueEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,44 +38,19 @@ public class MainActivity extends Activity {
 
     private void setupFirebaseDatabaseReference() {
         // TODO 03: use the FirebaseDatabase to get a DatabaseReference from the field "TEAM_ID"
-        databaseReference = firebaseDatabase.getReference(TEAM_ID);
     }
 
     private void destroyFirebaseDatabaseReference() {
         // TODO 04: remove the DatabaseReference
-        databaseReference = null;
     }
 
     private void setupButton() {
         // TODO 05: create a Button (located in package com.google.android.things.contrib.driver.button) for the button-gpio pin so that it is pressed when the signal is high
         // TODO 06: set an event listener to receive the button state when changed
-        try {
-            // high signal indicates the button is pressed (use with a pull-down resistor)
-            button = new Button(GPIO_BUTTON_PIN_NAME, Button.LogicState.PRESSED_WHEN_HIGH);
-            button.setOnButtonEventListener(new Button.OnButtonEventListener() {
-                @Override
-                public void onButtonEvent(Button button, boolean pressed) {
-                    Log.v(TAG, "onButtonEvent: pressed=" + pressed);
-                    databaseReference.setValue(pressed);
-                }
-            });
-        } catch (IOException e) {
-            Log.e(TAG, "Error configuring the button", e);
-        }
     }
 
     private void destroyButton() {
         // TODO 07: destroy and remove the button
-        if (button != null) {
-            try {
-                button.setOnButtonEventListener(null);
-                button.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Error closing button", e);
-            } finally {
-                button = null;
-            }
-        }
     }
 
     // TODO 08: at this point you should already be able to update your team-value in Firebase with the push of the button
@@ -99,58 +58,18 @@ public class MainActivity extends Activity {
     private void setupLed() {
         // TODO 09: create a gpio reference for the led using the PeripheralManagerService
         // TODO 10: set the gpio direction to out and initially low
-        PeripheralManagerService peripheralManagerService = new PeripheralManagerService();
-        try {
-            led = peripheralManagerService.openGpio(GPIO_LED_PIN_NAME);
-            led.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
-        } catch (IOException e) {
-            Log.e(TAG, "Error configuring the led", e);
-        }
     }
 
     private void destroyLed() {
         // TODO 11: destroy and remove the led
-        if (led != null) {
-            try {
-                led.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Error closing led", e);
-            } finally {
-                led = null;
-            }
-        }
     }
 
     private void setupFirebaseValueListener() {
         // TODO 12: add a ValueEventListener to the DatabaseReference to receive value updates
         // TODO 13: when receiving a value update set the new value to the led (hint: you may need to cast the value to boolean)
-        valueEventListener = databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // In general we would need to be more careful before casting the value,
-                // but in this example we know that this is of type boolean
-                boolean value = (boolean) dataSnapshot.getValue();
-                setLedValue(value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) { /* ignored */ }
-        });
-    }
-
-    private void setLedValue(boolean value) {
-        try {
-            led.setValue(value);
-        } catch (IOException e) {
-            Log.e(TAG, "Error setting led value", e);
-        }
     }
 
     private void destroyFirebaseValueListener() {
         // TODO 14: remove the ValueEventListener
-        if (valueEventListener != null) {
-            databaseReference.removeEventListener(valueEventListener);
-            valueEventListener = null;
-        }
     }
 }
